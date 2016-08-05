457fd03d62de8dc8f9ca4fddbb5d0a795570e7f6
sakuuat <taraponchu@gmail.com>
2016-08-05 14:44:08 +0900

    差分を加算するfor文を改良

--- kora.py
+++ kora.py
@@ -12,6 +12,7 @@ hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_red2 = np.array([175, 50, 50])
 upper_red2 = np.array([180,255, 255])
 
+
 #img_mask = cv2.inRange(hsv, lower_red, upper_red)
 img_mask = cv2.inRange(hsv, lower_red2, upper_red2)
 
@@ -35,18 +36,39 @@ for x in img_color[2]:
     for y in x:
         v_sum += y
 
-#count = len(np.where(gray_mask!=0)[2])
 count = cv2.countNonZero(gray_mask)
 ave_hue = h_sum/count
 ave_saturation = s_sum/count
 ave_value = v_sum/count
 
-print h_sum, s_sum, v_sum
-print "count=", count
-print ave_hue, ave_saturation, ave_value
+change_hue = 177-ave_hue
+change_saturation = 255-ave_saturation
+change_value = 255-ave_value
 
+for w in img:
+    for h in w:
+        if h[0].astype(np.float64)+change_hue > 180:
+            h[0] =180 
+        else:
+            h[0] += change_hue
+                       
+        if h[1].astype(np.float64)+change_saturation > 255:
+            h[1] = 255
+        else:
+            h[1] += change_saturation
+
+        if h[2].astype(np.float64)+change_value > 255:
+            h[2] = 255
+        else:
+            h[2] += change_value
+
+
+re_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+print ave_hue, ave_saturation, ave_value
+print change_hue, change_saturation, change_value
 
 #import pdb; pdb.set_trace()
-cv2.imshow('img',color_mask)
+cv2.imshow('img',img)
+cv2.imshow('re_img',re_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
